281da9eb3011a989e6230cdbe0e94230b03bc024
Randi Vertongen <r0460870@student.thomasmore.be>
2017-02-17 12:35:43 +0100

    juiste category_id aan toegevoegd (ik heb 6 aangewezen gekregen

--- index_Randi.js
+++ index_Randi.js
@@ -1,6 +1,6 @@
 var unirest = require('unirest');
 
-var categoryId = 0;
+var categoryId = 6;
 unirest
     .get('http://services.runescape.com/m=itemdb_rs/api/catalogue/category.json?category=' + categoryId)
     .as.json(handleResponse);
