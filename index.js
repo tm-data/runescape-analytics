fe927e096e15f3a35d2c6c7444b8c6f35e46532b
Daan Gerits <daan.gerits@gmail.com>
2017-02-24 12:02:14 +0100

    created the correct download implementation Removed Karwins data files.

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 var unirest = require('unirest');
+var Fetcher = require('./lib/fetcher');
+
+var fetcher = new Fetcher(6000);
 
 var categoryId = 0;
 unirest
     .get('http://services.runescape.com/m=itemdb_rs/api/catalogue/category.json?category=' + categoryId)
     .as.json(handleResponse);
 
-var requests = [];
-
 function handleResponse(response) {
     var body = JSON.parse(response.body);
 
     var alphabet = body.alpha;
-
-    console.log(alphabet);
 
     alphabet.forEach(function(item) {
         var number_of_records = item.items;
@@ -20,41 +19,20 @@ function handleResponse(response) {
         var number_of_pages = Math.ceil(number_of_records / 12);
 
         for (var pageIdx = 1; pageIdx <= number_of_pages; pageIdx++) {
-            fetchItemPageWrapper(categoryId, letter, pageIdx);
+            fetcher.push("http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=" + categoryId + "&alpha=" + letter + "&page=" + pageIdx, handleItemResponse);
         }
     });
 
     // -- start the processing of our requests
-    getNext();
+    fetcher.execute();
 }
 
+function handleItemResponse(response, page, letter, category) {
+    console.log("body: " + JSON.stringify(response));
 
-function getNext() {
-    if (requests.length == 0) return;
-    else {
-        var request = requests.pop();
+    fs.writeFile(category + "-" + letter + "-" + page + "-items.json", JSON.stringify(response), function(err) {
+        if (err) console.log(JSON.stringify(err));
 
-        request();
-    }
-}
-
-function fetchItemPageWrapper(category, letter, page) {
-    requests.push(function() {
-        fetchItemPage(category, letter, page)
+        console.out("We did it!");
     });
-}
-
-function fetchItemPage(category, letter, page) {
-    console.log("fetch page " + page + " for letter " + letter);
-    var url = "http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=" + category + "&alpha=" + letter + "&page=" + page;
-
-    unirest
-        .get(url)
-        .as.json(handleItemResponse);
-
-    setTimeout(getNext, 6000);
-}
-
-function handleItemResponse(response) {
-    console.log(response.body);
 }
