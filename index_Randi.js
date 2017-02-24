1b5392044b97b5e1dbb0dd8dfcf7a1246b5372ca
Randi Vertongen <r0460870@student.thomasmore.be>
2017-02-24 12:05:58 +0100

    Updated, eerste poging dat category id ook aangepast wordt, en door allee files loopt. (Allemaal Hard coded dus niet goed)

--- index_Randi.js
+++ index_Randi.js
@@ -1,5 +1,6 @@
 var unirest = require('unirest');
 
+var fs = require('fs');
 var categoryId = 8;
 unirest
     .get('http://services.runescape.com/m=itemdb_rs/api/catalogue/category.json?category=' + categoryId)
@@ -30,7 +31,7 @@ function handleResponse(response) {
 
 
 function getNext() {
-    if (requests.length == 0) return;
+    if (requests.length == 0) restart(categoryId);
     else {
         var request = requests.pop();
 
@@ -50,11 +51,73 @@ function fetchItemPage(category, letter, page) {
 
     unirest
         .get(url)
-        .as.json(handleItemResponse);
+        .as.json(function (response){return handleItemResponse(response, page, letter, category); });
 
     setTimeout(getNext, 6000);
 }
 
-function handleItemResponse(response) {
-    console.log(response.body);
+function handleItemResponse(response,page,letter,category) {
+    var test = JSON.parse(response.body)
+    fs.writeFile( 'data/'+category+ '-' + letter + '-' + page +'-test.JSON',JSON.stringify(test.items));
+}
+function restart(categoryId){
+categoryId++;
+    unirest
+        .get('http://services.runescape.com/m=itemdb_rs/api/catalogue/category.json?category=' + categoryId)
+        .as.json(handleResponse);
+    var requests = [];
+
+    function handleResponse(response) {
+        var body = JSON.parse(response.body);
+
+        var alphabet = body.alpha;
+
+        console.log(alphabet);
+
+        alphabet.forEach(function(item) {
+            var number_of_records = item.items;
+            var letter = item.letter;
+            var number_of_pages = Math.ceil(number_of_records / 12);
+
+            for (var pageIdx = 1; pageIdx <= number_of_pages; pageIdx++) {
+                fetchItemPageWrapper(categoryId, letter, pageIdx);
+            }
+        });
+
+        // -- start the processing of our requests
+        getNext();
+    }
+
+
+    function getNext() {
+        if (requests.length == 0) restart(categoryId);
+        else {
+            var request = requests.pop();
+
+            request();
+        }
+    }
+
+    function fetchItemPageWrapper(category, letter, page) {
+        requests.push(function() {
+            fetchItemPage(category, letter, page)
+        });
+    }
+
+    function fetchItemPage(category, letter, page) {
+        console.log("fetch page " + page + " for letter " + letter);
+        var url = "http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=" + category + "&alpha=" + letter + "&page=" + page;
+
+        unirest
+            .get(url)
+            .as.json(function (response){return handleItemResponse(response, page, letter, category); });
+
+        setTimeout(getNext, 6000);
+    }
+
+    function handleItemResponse(response,page,letter,category) {
+        var test = JSON.parse(response.body)
+        fs.writeFile( 'data/'+category+ '-' + letter + '-' + page +'-test.JSON',JSON.stringify(test.items));
+    }
+
 }
