3bfc262459403fc3fecb6acb5d624ae453efc975
Randi Vertongen <r0460870@student.thomasmore.be>
2017-03-03 12:00:14 +0100

    Items Volledig

--- clean.js
+++ clean.js
@@ -1,23 +1,28 @@
 var fs = require('./utils/fs-utils');
-
+var LineByLineReader = require('line-by-line');
+var elastic = require('elasticsearch')
 // -- cleanup.js
 // -- Clean the fetched data
 
-
+var client = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace'
+});
 
 // 1. read the data from the data/fetched folder.
-fs.readData('data/fetched', function(err, filename, json) {
-    json.forEach(function(item) {
-        var transformedItem = transform(item);
-
-        save(transformedItem);
+    lr = new LineByLineReader('./data/items.json');
+    lr.on('line', function (line) {
+        var item = JSON.parse(line)
+            var transformedItem = transform(item);
+            save(transformedItem);
     });
-});
+
+
 
 // 2. transform the data
 function transform(item) {
     // do the transformation
-    var price = item.current.price;
+    var price = JSON.parse(item).current.price;
     if (typeof(price) == 'string') {
         if (price.lastIndexOf('k') == price.length - 1) {
             var amount = price.substring(0, price.length - 1);
@@ -34,22 +39,7 @@ function transform(item) {
         }
     }
 
-    price = item.today.price;
-    if (typeof(price) == 'string') {
-        if (price.lastIndexOf('k') == price.length - 1) {
-            var amount = price.substring(0, price.length - 1);
-            item.today.price = amount * 1000;
-        } else if (price.lastIndexOf('K') == price.length - 1) {
-            var amount = price.substring(0, price.length - 1);
-            item.today.price = amount * 1000;
-        } else if (price.lastIndexOf('m') == price.length - 1) {
-            var amount = price.substring(0, price.length - 1);
-            item.today.price = amount * 1000 * 1000;
-        } else if (price.lastIndexOf('M') == price.length - 1) {
-            var amount = price.substring(0, price.length - 1);
-            item.today.price = amount * 1000 * 1000;
-        }
-    }
+
 
     delete item.icon;
     delete item.icon_large;
@@ -59,12 +49,15 @@ function transform(item) {
 }
 
 // 3. write the data to the file
-function save(item) {
-    fs.appendFile('data/items.jsl', JSON.stringify(item) + "\n", function(err) {
-        if (err) throw err;
+function save(item){
+  //  fs.appendFile('data/itemstest.json', JSON.stringify(line) + "\n", function(err) {
+   //     if (err) throw err;
 
+client.update({
+    index:'runescape-1'
+        mappings:''
         console.log('item written');
-    });
+});
 }
 
 
