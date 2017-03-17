0b40776f421912f1de834f75a144272ceb285851
Randi Vertongen <r0460870@student.thomasmore.be>
2017-03-17 10:45:41 +0100

    Wegschrijven werkt, wel nog foutje in categories: meerdere dezelfde categories

--- process/index2.js
+++ process/index2.js
@@ -14,28 +14,28 @@ function handleError(err) {
 function handleDone() {
     console.log("Processing Complete");
     fsu.appendToFile('data/categories6.json', categories);
-        console.log('item written');
+    console.log('item written');
 
     fsu.appendToFile('data/pricing5.json', prices);
 
-        console.log('item written');
+    console.log('item written');
 
     fsu.appendToFile('data/item5.json', items);
 
 }
 
 function handleItem(item) {
-     handlecategory(item);
-     handlepricing(item);
-     handleitems(item);
+    handlecategory(item);
+    handlepricing(item);
+    handleitems(item);
 }
 
 function handlecategory(item) {
-var category ={
-     categoryID : item.type.toLowerCase(),
-    name: item.type,
-    icon: item.typeIcon
-};
+    var category ={
+        categoryID : item.type.toLowerCase(),
+        name: item.type,
+        icon: item.typeIcon
+    };
     if (categories.search(category.categoryID) == -1) {
         categories.push(category);
         console.log('test3');
@@ -69,7 +69,7 @@ function handleitems(item){
     };
     console.log('test5');
     if (items.search(item.Name)==-1){
-       items.push(items2);
+        items.push(items2);
         console.log('test6');
     }
 
